2e719e2b942d9925959513913ae9bc69874f7a05
Lan Tian <xuyh0120@outlook.com>
2018-07-15 01:12:53 +0800

    Separate shell code from header file

--- dev/debug/serial_shell.cpp
+++ dev/debug/serial_shell.cpp
@@ -1,3 +1,34 @@
 #include "serial_shell.hpp"
 
 using namespace chibios_rt;
+
+static SerialConfig shellSerialConfig = {115200, 0, 0, 0};
+
+static const ShellConfig shellConfig = {
+    (BaseSequentialStream*) &SD1,
+    shellCommands
+};
+
+void SerialShellThread::main(void) {
+    // Do nothing, as this function will never be called.
+    // This function is here, otherwise G++ will be angry.
+    // SerialShellThread calls the thread provided by shell instead.
+}
+
+chibios_rt::ThreadReference SerialShellThread::start(tprio_t prio) {
+    // Remember to change GPIO pins here
+    palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
+    palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
+    sdStart(&SD1, &shellSerialConfig);
+    // Call init provided by shell
+    shellInit();
+    
+    // Create a thread and name it
+    thread_t *shellThreadRef = chThdCreateStatic(
+        wa, sizeof(wa), prio,
+        shellThread, (void*) &shellConfig);
+    chRegSetThreadNameX(shellThreadRef, "shell");
+
+    // Return reference to thread
+    return chibios_rt::ThreadReference(shellThreadRef);
+}
--- dev/debug/serial_shell.hpp
+++ dev/debug/serial_shell.hpp
@@ -12,28 +12,9 @@ class SerialShellThread : public chibios_rt::BaseThread {
 protected:
     THD_WORKING_AREA(wa, 1024);
 
-    SerialConfig shellSerialConfig = {115200, 0, 0, 0};
-
-    const ShellConfig shellConfig = {
-        (BaseSequentialStream*) &SD1,
-        shellCommands
-    };
-
-    void main(void) override {}
+    void main(void) override;
 public:
-    chibios_rt::ThreadReference start(tprio_t prio) override {
-        palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
-        palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
-        sdStart(&SD1, &shellSerialConfig);
-        shellInit();
-        
-        thread_t *shellThreadRef = chThdCreateStatic(
-            wa, sizeof(wa), prio,
-            shellThread, (void*) &shellConfig);
-        chRegSetThreadNameX(shellThreadRef, "shell");
-
-        return chibios_rt::ThreadReference(shellThreadRef);
-    }
+    chibios_rt::ThreadReference start(tprio_t prio) override;
 };
 
 static SerialShellThread serialShell;
