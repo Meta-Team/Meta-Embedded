751735e22bed6eb09da1169e3d7a85c0b63b1ab5
Lan Tian <xuyh0120@outlook.com>
2018-07-15 00:44:38 +0800

    Rewritten shell wrapper (More C++!)

--- dev/debug/serial_shell.cpp
+++ dev/debug/serial_shell.cpp
@@ -1,24 +1,3 @@
 #include "serial_shell.hpp"
 
 using namespace chibios_rt;
-
-static SerialConfig shellSerialConfig = {115200, 0, 0, 0};
-
-static const ShellConfig shellConfig = {
-    (BaseSequentialStream*) &SD1,
-    shellCommands
-};
-
-static THD_WORKING_AREA(waShell, 1024);
-
-void shellStart(void) {
-    palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
-    palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
-    sdStart(&SD1, &shellSerialConfig);
-    shellInit();
-    
-    thread_t *shellThreadRef = chThdCreateStatic(
-        waShell, sizeof(waShell), NORMALPRIO,
-        shellThread, (void*) &shellConfig);
-    chRegSetThreadNameX(shellThreadRef, "shell");
-}
--- dev/debug/serial_shell.hpp
+++ dev/debug/serial_shell.hpp
@@ -8,6 +8,34 @@
 
 #include "serial_shell_commands.hpp"
 
-void shellStart(void);
+class SerialShellThread : public chibios_rt::BaseThread {
+protected:
+    THD_WORKING_AREA(wa, 1024);
+
+    SerialConfig shellSerialConfig = {115200, 0, 0, 0};
+
+    const ShellConfig shellConfig = {
+        (BaseSequentialStream*) &SD1,
+        shellCommands
+    };
+
+    void main(void) override {}
+public:
+    chibios_rt::ThreadReference start(tprio_t prio) override {
+        palSetPadMode(GPIOA, 9, PAL_MODE_ALTERNATE(7));
+        palSetPadMode(GPIOA, 10, PAL_MODE_ALTERNATE(7));
+        sdStart(&SD1, &shellSerialConfig);
+        shellInit();
+        
+        thread_t *shellThreadRef = chThdCreateStatic(
+            wa, sizeof(wa), prio,
+            shellThread, (void*) &shellConfig);
+        chRegSetThreadNameX(shellThreadRef, "shell");
+
+        return chibios_rt::ThreadReference(shellThreadRef);
+    }
+};
+
+static SerialShellThread serialShell;
 
 #endif
