42a94542b7c66de7b13b53324c0aa71980c6c627
Lan Tian <xuyh0120@outlook.com>
2018-07-15 02:04:16 +0800

    Separate shell config to another file

---
+++ dev/shellconf.h
@@ -0,0 +1,14 @@
+#ifndef _SHELLCONF_H_
+#define _SHELLCONF_H_
+
+/*===========================================================================*/
+/* ChibiOS Shell related settings.                                           */
+/*===========================================================================*/
+
+// Disable shell test commands, so we don't rely on ChibiOS's test codes
+#define SHELL_CMD_TEST_ENABLED              FALSE
+
+// Disable exiting the shell
+#define SHELL_CMD_EXIT_ENABLED              FALSE
+
+#endif
