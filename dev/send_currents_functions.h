0a851c3bc8eb6dd14db37a7a01e2ffa8a62b96b0
Fengch365 <d62375@126.com>
2018-12-15 21:37:34 +0800

    I add some declaration to the class

--- dev/send_currents_functions.h
+++ dev/send_currents_functions.h
@@ -5,9 +5,31 @@
 #ifndef META_INFANTRY_SEND_CURRENTS_FUNCTIONS_H
 #define META_INFANTRY_SEND_CURRENTS_FUNCTIONS_H
 
+#define GIMBAL_MOTOR_YAW 0
+#define GIMBAL_MOTOR_PIT 1
+// need exact value to define
+#define GIMBAL_MOTOR_MAX_CURRENT
+#define CHASSIS_MOTOR_MAX_CURRENT
+
 class Send_Currents{
 
     public:
+    // type definition
+    typedef struct motor_t motor_t;
+    typedef struct platform_t platform_t;
+    typedef struct shoot_mechanism_t shoot_mechanism_t;
+
+    struct motor_t{
+        int16_t target_current;
+    };
+    struct platform_t{
+        motor_t motor[4];
+    };
+    platform_t chassis, gimbal;
+    struct shoot_mechanism_t{
+        int16_t stir_current;
+    };
+    shoot_mechanism_t shoot_mechanism;
 
     CANTxFrame txmsg_chassis; // create a new data structure only for chassis
     txmsg_chassis.IDE = CAN_IDE_STD; // variable declaration
@@ -15,7 +37,7 @@ class Send_Currents{
     txmsg_chassis.RTR = CAN_RTR_DATA;
     txmsg_chassis.DLC = 0x08;
 
-    void send_chassis_currents(void); // function for sending current signals to the chassis
+    void send_chassis_currents(); // function for sending current signals to the chassis
 
     CANTxFrame txmsg_gimbal; // create a new data structure only for gimbal
     txmsg_gimbal.IDE = CAN_IDE_STD; // variable declaration
@@ -23,7 +45,7 @@ class Send_Currents{
     txmsg_gimbal.RTR = CAN_RTR_DATA;
     txmsg_gimbal.DLC = 0x08;
 
-    void send_gimbal_currents(void); // function for sending current signals to the gimbal
+    void send_gimbal_currents(); // function for sending current signals to the gimbal
 
 };
 
