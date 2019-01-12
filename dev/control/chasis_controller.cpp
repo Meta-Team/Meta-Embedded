dc3cde18e25995da63741c0c8e008e09d3858c81
YeAnbang <anbang.17@intl.zju.edu.cn>
2019-01-12 19:07:39 +0800

    under test chasis controller
    under test chasis controller

--- dev/control/chasis_controller.cpp
+++ dev/control/chasis_controller.cpp
@@ -14,78 +14,61 @@ using namespace chibios_rt;
 
 void chasisPidController::chasis_pid_change_parameters(float kp_v_loop, float ki_v_loop, float kd_v_loop, float i_limit,
                                                       float out_limit) {
-/*
- * Param:
- *      chasis_PIDController: chasis_PIDController structure pointer to change parameters
- *      kp_ang_w_loop: angular movement kp for angular velocity
- *      ki_ang_w_loop: angular movement ki for angular velocity
- *      kd_ang_w_loop: angular movement kd for angular velocity
- *      kp_v_loop: velocity loop kp
- *      ki_v_loop: velocity loop ki
- *      kd_v_loop: velocity loop kd
- *      w_limit: angular velocity limit
- *      out_limit: the output current limit
+/**
+ * @brief   change chassis pid params
+ * @param   chassis_PIDController: chassis_PIDController structure pointer to change parameters
+ * @param   kp_angular_w_loop: angular movement kp for angular velocity
+ * @param   ki_angular_w_loop: angular movement ki for angular velocity
+ * @param   kd_angular_w_loop: angular movement kd for angular velocity
+ * @param   kp_v_loop: velocity loop kp
+ * @param   ki_v_loop: velocity loop ki
+ * @param   kd_v_loop: velocity loop kd
+ * @param   w_limit: angular velocity limit
+ * @param   out_limit: the output current limit
  *
- * Func:
- *      setup a new chasis_controller with following parameters
- *
- * Return
- *      the pointer to the new allocated chasis_controller
  */
     change_parameters(kp_v_loop, ki_v_loop, kd_v_loop, i_limit, out_limit);
 }
 
 
 void chasisPidController::chasis_pid_calc(float measured_velocity_X, float measured_velocity_Y,
-                                                  float measured_ang, float target_velocity_X, float target_velocity_Y,
+                                                  float measured_angle, float target_velocity_X, float target_velocity_Y,
                                                   float target_angular_velocity) {
-    /*
-     * Param:
-     *      chasisPidController: pointer to a chasis_PIDController structure
-     *      measured_velocity_X: measured velocity along the x axis with respect to the front of the chasis
-     *      measured_velocity_Y: measured velocity along the y axis with respect to the front of the chasis
-     *      measured_ang: measured angular with respect to the front of the chasis ( form -180 to 180)
-     *      target_velocity_X: target velocity along the x axis with respect to the front of the chasis
-     *      target_velocity_Y: target velocity along the y axis with respect to the front of the chasis
-     *      target_ang: target angular with respect to the front of the chasis
+    /**
+     * @brief   calculate current for all chassis motors
+     * @param   measured_velocity_X: measured velocity along the x axis with respect to the front of the chassis (mm/s)
+     * @param   measured_velocity_Y: measured velocity along the y axis with respect to the front of the chassis (mm/s)
+     * @param   measured_angle: measured angular with respect to the front of the chassis ( form -max_angle to max_angle, negative value for clockwise rotation) (degree/s)
+     * @param   target_velocity_X: target velocity along the x axis with respect to the front of the chassis (mm/s)
+     * @param   target_velocity_Y: target velocity along the y axis with respect to the front of the chassis (mm/s)
+     * @param   target_angular_velocity: target angular with respect to the front of the chassis (degree/s, negative value for clockwise rotation)
      */
-    float d_ang;
-    if ((measured_ang - chasis_ang) > max_ang * 0.7) {
-        d_ang = measured_ang - chasis_ang - max_ang*2;
-    }
-    else if ((measured_ang - chasis_ang) < -max_ang * 0.7) {
-        d_ang = 360 - measured_ang - chasis_ang;
-    }
-    else d_ang = measured_ang - chasis_ang;
-    float real_ang_velocity = d_ang / max_ang * 180 / chasisl_thread_interval * 1000.0f;
-    chasis_motor_I_front_left = calc(measured_velocity_Y + measured_velocity_X - real_ang_velocity * front_rotate_ratio,
-                                                                target_velocity_Y + target_velocity_X - target_angular_velocity * front_rotate_ratio);
+    float real_angular_velocity = (measured_angle - chasis_angle) / max_ang * 180 / chassis_thread_interval * 1000.0f;
+    output[0] = calc(measured_velocity_Y + measured_velocity_X - real_angular_velocity * front_rotate_ratio,
+                                                                target_velocity_Y + target_velocity_X - target_angular_velocity * front_rotate_ratio)*front_left_current_ratio*CHASSIS_WHEEL_RATIO;
 
-    chasis_motor_I_front_right = calc(measured_velocity_Y - measured_velocity_X + real_ang_velocity * front_rotate_ratio,
-                                                                 target_velocity_Y - target_velocity_X + target_angular_velocity * front_rotate_ratio);
+    output[1] = calc(measured_velocity_Y - measured_velocity_X + real_angular_velocity * front_rotate_ratio,
+                                                                 target_velocity_Y - target_velocity_X + target_angular_velocity * front_rotate_ratio)*front_right_current_ratio*CHASSIS_WHEEL_RATIO;
 
-    chasis_motor_I_back_left = calc(measured_velocity_Y - measured_velocity_X  - real_ang_velocity * back_rotate_ratio,
-                                                               target_velocity_Y - target_velocity_X - target_angular_velocity *back_rotate_ratio);
+    output[2] = calc(measured_velocity_Y - measured_velocity_X  - real_angular_velocity * back_rotate_ratio,
+                                                               target_velocity_Y - target_velocity_X - target_angular_velocity *back_rotate_ratio)*back_left_current_ratio*CHASSIS_WHEEL_RATIO;
 
-    chasis_motor_I_back_right = calc(measured_velocity_Y + measured_velocity_X + real_ang_velocity * back_rotate_ratio,
-                                                                target_velocity_Y + target_velocity_X + target_angular_velocity * back_rotate_ratio);
+    output[3] = calc(measured_velocity_Y + measured_velocity_X + real_angular_velocity * back_rotate_ratio,
+                                                                target_velocity_Y + target_velocity_X + target_angleule_velocity * back_rotate_ratio)*back_right_current_ratio*CHASSIS_WHEEL_RATIO;
 
-    chasis_ang = measured_ang;
-
-    read_all_current(output);
+    chasis_angle = measured_angle;
 }
 
-void chasisPidController::chasis_pid_set_front(float measured_ang) {
-    front_ang = measured_ang;
+void chasisPidController::chasis_pid_set_front(float measured_angle) {
+    /**
+     * @brief   set front ang
+     */
+    front_angle = measured_angle;
 }
 
-float chasisPidController::read_front_ang() {
-    return front_ang;
-}
-
-void chasisPidController::read_all_current(float *current_array) {
-    current_array[0] = chasis_motor_I_front_left;    //target current for front left motor
-    current_array[1] = chasis_motor_I_front_right;   //target current for front right motor
-    current_array[2] = chasis_motor_I_back_left; //target current for back left motor
-    current_array[3] = chasis_motor_I_back_right; //target current for back right motor
+float chasisPidController::read_front_angle() {
+    /**
+     * @brief   read front ang
+     */
+    return front_angle;
 }
--- dev/control/chasis_controller.h
+++ dev/control/chasis_controller.h
@@ -6,83 +6,59 @@
 #define PROJECT_CHASIS_CONTROLLER_H
 #include "pid_controller.h"
 
-#define MY_INFINITY 10000.0
-
 /********** Chassis Structure Parameters **********/
-#define CHASSIS_WHEEL_PERIMETER  478 //mm
+float const CHASSIS_WHEEL_RATIO  478.0f/2.0f //mm
+/* wheel track distance(mm) */
+float const CHASSIS_WHEELTRACK 358
+/* wheelbase distance(mm) */
+float const CHASSIS_WHEELBASE  358
 
-/* the deceleration ratio of chassis motor */
-#define CHASSIS_DECELE_RATIO (1.0f/19.0f)
-
-
-float const chasisl_thread_interval = 50; // ms
+float const chassis_thread_interval = 50; // ms
 
 float const maximum_current = 4000; // mA
 
-float const max_ang = 180;    //max angle read by the electronic speed controller (Please change it for your convenience!!)
+/**
+ * parameters below should be based on experiment value
+ * for each motor, XX_XX_current_ratio = angular velocity change / current change
+ */
+float const front_left_current_ratio = 0.0f;  //TODO: Please change this value
 
-float const front_wheel_x = 0; //Please change this value
+float const front_right_current_ratio = 0.0f;   //TODO: Please change this value
 
-float const front_wheel_y = 0; //Please change this value
+float const back_left_current_ratio = 0.0f;     //TODO: Please change this value
 
-float const behind_wheel_x = 0; //Please change this value
-
-float const behind_wheel_y = 0; //Please change this value
-
-float const chasis_i_limit = 3000.0f;
-
-float const chasis_max_ang_velocity = 1; //degree per ms
+float const back_right_current_ratio = 0.0f;    //TODO: Please change this value
 
 
 class chasisPidController: public PIDController{
 private:
-    float *output;
-    float front_ang;
-    float chasis_ang;
-    float front_rotate_ratio;
-    float back_rotate_ratio;
-    float current_ratio;
-    float chasis_motor_I_front_left;    //target current for front left motor
-    float chasis_motor_I_front_right;   //target current for front right motor
-    float chasis_motor_I_back_left; //target current for back left motor
-    float chasis_motor_I_back_right;    //target current for back right motor
-
+    float front_angle;    //[chasis_motor_I_front_left, chasis_motor_I_front_right, chasis_motor_I_front_left, chasis_motor_I_back_right]
+    float chasis_angle;    //chasis_angle with respect to front direction
 public:
+    float output[4];
     chasisPidController():PIDController() {}
 
-    chasisPidController(float* output_array):PIDController(){
-        output = output_array;
-    }
-
-    chasisPidController(float* output_array, float kp_v_loop, float ki_v_loop, float kd_v_loop, float i_limti,
-                        float out_limit, float current_ang): PIDController(kp_v_loop, ki_v_loop, kd_v_loop, i_limti, out_limit) {
-        /*
- * Param:
- *      kp_ang_w_loop: angular movement kp for angular velocity
- *      ki_ang_w_loop: angular movement ki for angular velocity
- *      kd_ang_w_loop: angular movement kd for angular velocity
- *      kp_v_loop: velocity loop kp
- *      ki_v_loop: velocity loop ki
- *      kd_v_loop: velocity loop kd
- *      w_limit: angular velocity limit
- *      out_limit: the output current limit
- *      current_ang: current absolute ang when setup
- * Func:
- *      setup a new chasis_controller with following parameters*/
-        output = output_array;
-        front_rotate_ratio = front_wheel_x + front_wheel_y;
-        back_rotate_ratio = behind_wheel_x + behind_wheel_y;
-        current_ratio = (60.0f / (CHASSIS_WHEEL_PERIMETER * CHASSIS_DECELE_RATIO));
-        chasis_motor_I_front_left = chasis_motor_I_front_right = chasis_motor_I_back_left = chasis_motor_I_back_right = 0.0f;
-        chasis_pid_set_front(current_ang);
-        chasis_ang = current_ang;
+    chasisPidController(float kp_v_loop, float ki_v_loop, float kd_v_loop, float i_limti,
+                        float out_limit, float current_angle): PIDController(kp_v_loop, ki_v_loop, kd_v_loop, i_limti, out_limit) {
+        /**
+         * @brief   setup chassis controller
+         * @param   kp_v_loop: velocity loop kp
+         * @param   ki_v_loop: velocity loop ki
+         * @param   kd_v_loop: velocity loop kd
+         * @param   i_limit: i limit
+         * @param   out_limit: the output current limit
+         * @param   current_angle   current ang
+         *
+         */
+        rotate_ratio = (CHASSIS_WHEELBASE + CHASSIS_WHEELTRACK)/2.0f;
+        chasis_pid_set_front(current_angle);
+        chasis_angle = current_angle;
     }
     void chasis_pid_change_parameters(float kp_v_loop, float ki_v_loop, float kd_v_loop, float i_limit,float out_limit);
-    void chasis_pid_calc(float measured_velocity_X, float measured_velocity_Y, float measured_ang, float target_velocity_X,
+    void chasis_pid_calc(float measured_velocity_X, float measured_velocity_Y, float measured_angle, float target_velocity_X,
                                  float target_velocity_Y, float target_angular_velocity);
-    void chasis_pid_set_front(float measured_ang);
-    float read_front_ang();
-    void read_all_current(float *current_array);
+    void chasis_pid_set_front(float measured_angle);
+    float read_front_angle();
 };
 
 #endif //PROJECT_CHASIS_CONTROLLER_H
