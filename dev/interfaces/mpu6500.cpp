12a6d74779f2d1a2b164af0ebb62c9d4ca3f85ef
yjsxuanyuan <yjsxuanyuan@126.com>
2019-01-14 13:06:47 +0900

    add mpu6500 (not finished)

---
+++ dev/interfaces/mpu6500.cpp
@@ -0,0 +1,72 @@
+#include "mpu6500.h"
+#include <cmath>
+
+#define GRAV 9.80665
+#define MPU6500_RX_BUF_SIZE 0x0E
+#define MPU6500_TX_BUF_SIZE 0x05
+
+static uint8_t imuRXData[MPU6500_RX_BUF_SIZE];
+static uint8_t imuTXData[MPU6500_TX_BUF_SIZE];
+
+void IMUController::IMUInit() {
+    // read to set bias here
+    getData();
+}
+
+void IMUController::getData() {
+    uint32_t current_time =  chVTGetSystemTimeX();
+    this->dt = ST2US(current_time - this->prev_t)/1000000.0f;
+    this->prev_t = current_time;
+    // get dt
+
+    // get the cofficient converting the raw data to degree
+    float _gyro_psc;
+    switch(this->_gyro_config)
+    {
+        case MPU6500_GYRO_SCALE_250:
+            _gyro_psc = (1.0f / 131.0f) * M_PI/180.0f;
+            break;
+        case MPU6500_GYRO_SCALE_500:
+            _gyro_psc = (1.0f /  65.5f) * M_PI/180.0f;
+            break;
+        case MPU6500_GYRO_SCALE_1000:
+            _gyro_psc = (1.0f /  32.8f) * M_PI/180.0f;
+            break;
+        case MPU6500_GYRO_SCALE_2000:
+            _gyro_psc = (1.0f /  16.4f) * M_PI/180.0f;
+            break;
+    }
+
+    float _accel_psc;
+    switch(this->_accel_config)
+    {
+        case MPU6500_ACCEL_SCALE_2G:
+            _accel_psc = (GRAV / 16384.0f);
+            break;
+        case MPU6500_ACCEL_SCALE_4G:
+            _accel_psc = (GRAV /  8192.0f);
+            break;
+        case MPU6500_ACCEL_SCALE_8G:
+            _accel_psc = (GRAV /  4096.0f);
+            break;
+        case MPU6500_ACCEL_SCALE_16G:
+            _accel_psc = (GRAV /  2048.0f);
+            break;
+    }
+    /*Here to read data*/
+
+    float accel_x = _accel_psc * (int16_t)((imuRXData[ 0]<<8) | imuRXData[ 1]); // Accel X 
+    float accel_y = _accel_psc * (int16_t)((imuRXData[ 2]<<8) | imuRXData[ 3]); // Accel Y 
+    float accel_z = _accel_psc * (int16_t)((imuRXData[ 4]<<8) | imuRXData[ 5]); // Accel Z
+    float gyro_x = _gyro_psc * (int16_t)((imuRXData[ 8]<<8) | imuRXData[ 9]);  // Gyro X 
+    float gyro_y = _gyro_psc * (int16_t)((imuRXData[10]<<8) | imuRXData[11]);  // Gyro Y 
+    float gyro_z = _gyro_psc * (int16_t)((imuRXData[12]<<8) | imuRXData[13]);  // Gyro Z 
+
+    this->gyro_data.x = (gyro_x + _gyro_bias) / this->dt;
+    this->gyro_data.y = (gyro_y + _gyro_bias) / this->dt;
+    this->gyro_data.z = (gyro_z + _gyro_bias) / this->dt;
+
+    Matrix accel_data_matrix(1, 3);
+    // fill the matrix with accel_x, accel_y and accel_z
+    this->accel_data = matrix_multiply_to_accelerate_t(accel_data_matrix, _accel_bias);
+}
---
+++ dev/interfaces/mpu6500.h
@@ -0,0 +1,82 @@
+#ifndef META_MPU6500_H_
+#define META_MPU6500_H_
+
+typedef struct {
+    float wx;
+    float wy;
+    float wz;
+} angel_speed_t;
+
+typedef struct {
+    float ax;
+    float ay;
+    float az;
+} accelerate_t;
+
+/*typedef struct {
+    float q0;
+    float q1;
+    float q2;
+    float q3;
+} quaternion_t;*/
+
+class IMUController {
+
+public:
+
+    /*
+    * Scale config for gyro
+    */
+    typedef enum {
+        MPU6500_GYRO_SCALE_250 = 0,  // range of 250 with sensitivity factor 131
+        MPU6500_GYRO_SCALE_500 = 1, // range of 500 with sensitivity factor 65.5
+        MPU6500_GYRO_SCALE_1000 = 2, // range of 1000 with sensitivity factor 32.8
+        MPU6500_GYRO_SCALE_2000 = 3 // range of 1000 with sensitivity factor 16.4
+    } gyro_scale;
+
+    /*
+    * Scale config for acceleration
+    */
+    typedef enum {
+        MPU6500_ACCEL_SCALE_2G = 0,
+        MPU6500_ACCEL_SCALE_4G = 1,
+        MPU6500_ACCEL_SCALE_8G = 2,
+        MPU6500_ACCEL_SCALE_16G = 3
+    } accel_scale;
+
+    /**
+    * @brief read data from mpu6000 and convert to angel_speed_t type
+    * @param none
+    * @return an angel_speed_t type
+    * @note this function is temporary for gyro. Later we should add acceleration to it.
+    */
+    void getData();
+
+    IMUController(gyro_scale input_gyro_config, accel_scale input_accel_config):_accel_bias(3, 3) {
+/*        imu_q = {1, 0 ,0 ,0};*/
+        _gyro_config = input_gyro_config;
+        _accel_config = input_accel_config;
+        dt = 0;
+        IMUInit();
+    }
+
+private:
+
+    float dt;
+    float prev_t;
+
+    gyro_scale _gyro_config;
+    accel_scale _accel_config
+
+    float _gyro_bias;  // for gyro bias
+    Matrix _accel_bias;  // a matrix for accel bias (need the support of matrix)
+
+/*    quaternion_t imu_q;*/
+
+    angel_speed_t gyro_data;  // final data of gyro
+    accelerate_t accel_data;  // final data of acceleration
+
+    void IMUInit();
+}
+
+#endif
