a743dc156bd42fe671feaaf5f13b56f86c01949a
cenzhi <zhi.17@intl.zju.edu.cn>
2019-01-16 13:32:43 +0800

    Add mode using ist8300 to modify angular speeds but also can be used when there is no input from ist8300. Still need input from ist8310 and dt(now assum dt are the same both in mpu6500 and ist8310). Things that need modifying can be found by TODO comments

--- dev/interfaces/attitude_calc.h
+++ dev/interfaces/attitude_calc.h
@@ -1,9 +1,12 @@
 #ifndef MATA_ATTITUDE_CALC_H
 #define MATA_ATTITUDE_CALC_H
 
+
+// output
 struct quaternion_t {
     float q0, q1, q2, q3;
 } quaternion_output;
+
 
 class BoardAttitude {
 
@@ -11,14 +14,18 @@ public:
 
     quaternion_t return_quaternion();
     // init pid
-    BoardAttitude(int p, int i, int d): kp(p), ki(i), kd(d) {
+    BoardAttitude(int p, int i, int d): kp(p), ki(i), kd(d), dt(0) {
         BoardAttitude();
     }
     
 private:
+
     // input
-    float w[3];
-    float a[3];
+    float w[3]; // angular speed
+    float a[3]; // acceleration
+    float m[3]; // magnetic field
+    // TODO: assume dt are same in imu6500 and ist8310
+    float dt;
 
     // quaternion
     float q[4];
@@ -32,15 +39,19 @@ private:
 
     // init q, b
     BoardAttitude() {
+        // init m[3]
+        m[0] = m[1] = m[2] = 0;
         // init q[3]
         q[0] = 1.0f;
         q[1] = q[2] = q[3] = 0.0f;
         // init integral[3]
         integral[0] = integral[1] = integral[2] = 0.0f;
     };
+
     // update quaternion
     void quaternion_update();
+    void with_ist8310();
+    void without_ist8310();
 };
-
 
 #endif // MATA_ATTITUDE_CALC_H
