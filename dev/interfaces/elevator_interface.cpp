4083340d0bdc8e8995db82ac8d171d8fe87cc62a
ZhuKerui <kerui.17@intl.zju.edu.cn>
2019-01-18 00:51:26 +0800

    add the "start" function

--- dev/interfaces/elevator_interface.cpp
+++ dev/interfaces/elevator_interface.cpp
@@ -6,26 +6,16 @@
 
 bool ElevatorInterface::send_message() {
     for(int i = 0; i < 2; i++) {
-        if (enabled) {
-            txFrames[i * 2].data8[0] = txFrames[i * 2 + 1].data8[0] = (unsigned char) ((PWM >> 8) & 0Xff);
-            txFrames[i * 2].data8[1] = txFrames[i * 2 + 1].data8[1] = (unsigned char) ((PWM) & 0Xff);
-            txFrames[i * 2].data8[2] = txFrames[i * 2].data8[3] = txFrames[i * 2 + 1].data8[2] = txFrames[i * 2 +
-                                                                                                          1].data8[3] = 0x55;
-            txFrames[i * 2].data8[4] = txFrames[i * 2 + 1].data8[4] = (unsigned char) ((target_position[i] >> 24) &
-                                                                                       0Xff);
-            txFrames[i * 2].data8[5] = txFrames[i * 2 + 1].data8[5] = (unsigned char) ((target_position[i] >> 16) &
-                                                                                       0Xff);
-            txFrames[i * 2].data8[6] = txFrames[i * 2 + 1].data8[6] = (unsigned char) ((target_position[i] >> 8) &
-                                                                                       0Xff);
-            txFrames[i * 2].data8[7] = txFrames[i * 2 + 1].data8[7] = (unsigned char) (target_position[i] & 0Xff);
-        } else {
-            /*do something when enabled is false*/
-        }
+        txFrames[i * 2].data8[4] = txFrames[i * 2 + 1].data8[4] = (unsigned char) ((target_position[i] >> 24) &
+                                                                                   0Xff);
+        txFrames[i * 2].data8[5] = txFrames[i * 2 + 1].data8[5] = (unsigned char) ((target_position[i] >> 16) &
+                                                                                   0Xff);
+        txFrames[i * 2].data8[6] = txFrames[i * 2 + 1].data8[6] = (unsigned char) ((target_position[i] >> 8) &
+                                                                                   0Xff);
+        txFrames[i * 2].data8[7] = txFrames[i * 2 + 1].data8[7] = (unsigned char) (target_position[i] & 0Xff);
+        can->send_msg(&txFrames[i * 2]);
+        can->send_msg(&txFrames[i * 2 + 1]);
     }
-    can->send_msg(&txFrames[0]);
-    can->send_msg(&txFrames[1]);
-    can->send_msg(&txFrames[2]);
-    can->send_msg(&txFrames[3]);
 }
 
 void ElevatorInterface::set_position(int32_t front_wheel_position, int32_t rear_wheel_position) {
@@ -34,25 +24,73 @@ void ElevatorInterface::set_position(int32_t front_wheel_position, int32_t rear_
 }
 
 bool ElevatorInterface::get_feedback(CANRxFrame *rxmsg) {
-    int id;
+    int index;
     switch (rxmsg->SID){
-        case FRONT_LEFT:
-            id = 0;
+        case RX_FRONT_LEFT:
+            index = 0;
             break;
-        case FRONT_RIGHT:
-            id = 1;
+        case RX_FRONT_RIGHT:
+            index = 1;
             break;
-        case REAR_LEFT:
-            id = 2;
+        case RX_REAR_LEFT:
+            index = 2;
             break;
-        case REAR_RIGHT:
-            id = 3;
+        case RX_REAR_RIGHT:
+            index = 3;
             break;
         default:
-            id = 0;
+            return false;
     }
-    elevator_wheels[id].real_current = (rxmsg.data8[0]<<8)|rxmsg.data8[1];
-    elevator_wheels[id].real_velocity = (rxmsg.data8[2]<<8)|rxmsg.data8[3];
-    elevator_wheels[id].real_position = (rxmsg.data8[4]<<24)|(rxmsg.data8[5]<<16)|(rxmsg.data8[6]<<8)|rxmsg.data8[7];
+    elevator_wheels[index].real_current = (rxmsg.data8[0]<<8)|rxmsg.data8[1];
+    elevator_wheels[index].real_velocity = (rxmsg.data8[2]<<8)|rxmsg.data8[3];
+    elevator_wheels[index].real_position = (rxmsg.data8[4]<<24)|(rxmsg.data8[5]<<16)|(rxmsg.data8[6]<<8)|rxmsg.data8[7];
     return true;
+}
+
+void ElevatorInterface::start(CANInterface* can_interface) {
+    can = can_interface;
+
+    // Reset the four wheels
+    txFrames[0].SID = COMMAND_0_FRONT_LEFT;
+    txFrames[1].SID = COMMAND_0_FRONT_RIGHT;
+    txFrames[2].SID = COMMAND_0_REAR_LEFT;
+    txFrames[3].SID = COMMAND_0_REAR_RIGHT;
+
+    for(int wheel_index = 0; wheel_index < 4; wheel_index++){
+        txFrames[wheel_index].IDE = CAN_IDE_STD;
+        txFrames[wheel_index].RTR = CAN_RTR_DATA;
+        txFrames[wheel_index].DLC = 0x08;
+        for(int data_index = 0; data_index < 8; data_index++){
+            txFrames[wheel_index].data8[data_index] = 0x55;
+        }
+        can->send_msg(&txFrames[wheel_index]);
+    }
+
+    // Waiting for the work to be done
+    chThdSleepMilliseconds(600);
+
+    // Choose the control mode for the four wheels
+    txFrames[0].SID = COMMAND_1_FRONT_LEFT;
+    txFrames[1].SID = COMMAND_1_FRONT_RIGHT;
+    txFrames[2].SID = COMMAND_1_REAR_LEFT;
+    txFrames[3].SID = COMMAND_1_REAR_RIGHT;
+
+    for(int wheel_index = 0; wheel_index < 4; wheel_index++){
+        txFrames[wheel_index].data8[0] = 0x05;
+        can->send_msg(&txFrames[wheel_index]);
+    }
+
+    // Waiting for the work to be done
+    chThdSleepMilliseconds(100);
+
+    // Set the PWM for the four wheels
+    txFrames[0].SID = COMMAND_5_FRONT_LEFT;
+    txFrames[1].SID = COMMAND_5_FRONT_RIGHT;
+    txFrames[2].SID = COMMAND_5_REAR_LEFT;
+    txFrames[3].SID = COMMAND_5_REAR_RIGHT;
+
+    for(int wheel_index = 0; wheel_index < 4; wheel_index++){
+        txFrames[wheel_index].data8[0] = (unsigned char) ((PWM >> 8) & 0Xff);
+        txFrames[wheel_index].data8[1] = (unsigned char) (PWM & 0Xff);
+    }
 }
--- dev/interfaces/elevator_interface.h
+++ dev/interfaces/elevator_interface.h
@@ -12,35 +12,68 @@
 class ElevatorInterface {
 public:
 
+    /**
+     * @brief the SID of the four CANTxFrames for the four wheels
+     */
     typedef enum {
-        FRONT_LEFT = 0x005,  // The front-left wheel (the id is temporary)
-        FRONT_RIGHT = 0x015,  // The front-right wheel (the id is temporary)
-        REAR_LEFT = 0x025,  // The left-rear wheel (the id is temporary)
-        REAR_RIGHT = 0x035  // The right-rear wheel (the id is temporary)
-    } elevator_wheel_id_t;
+        RX_FRONT_LEFT = 0x30B,  // The front-left wheel
+        RX_FRONT_RIGHT = 0x31B,  // The front-right wheel
+        RX_REAR_LEFT = 0x32B,  // The left-rear wheel
+        RX_REAR_RIGHT = 0x33B  // The right-rear wheel
+    } elevator_wheel_rx_id_t;
 
-    bool enabled;  // if not enabled, 0 current will be sent in send_elevator_currents
+    /**
+     * @brief the SID for command 5 (set position command) of the four CANRxFrames for the four wheels
+     */
+    typedef enum {
+        COMMAND_5_FRONT_LEFT = 0x305,  // The front-left wheel
+        COMMAND_5_FRONT_RIGHT = 0x315,  // The front-right wheel
+        COMMAND_5_REAR_LEFT = 0x325,  // The left-rear wheel
+        COMMAND_5_REAR_RIGHT = 0x335  // The right-rear wheel
+    } elevator_wheel_tx_command_5_id_t;
+
+    /**
+     * @brief the SID for command 0 (reset command) of the four CANRxFrames for the four wheels
+     */
+    typedef enum {
+        COMMAND_0_FRONT_LEFT = 0x300,  // The front-left wheel
+        COMMAND_0_FRONT_RIGHT = 0x310,  // The front-right wheel
+        COMMAND_0_REAR_LEFT = 0x320,  // The left-rear wheel
+        COMMAND_0_REAR_RIGHT = 0x330  // The right-rear wheel
+    } elevator_wheel_tx_command_0_id_t;
+
+    /**
+     * @brief the SID for command 1 (choose mode command) of the four CANRxFrames for the four wheels
+     */
+    typedef enum {
+        COMMAND_1_FRONT_LEFT = 0x301,  // The front-left wheel
+        COMMAND_1_FRONT_RIGHT = 0x311,  // The front-right wheel
+        COMMAND_1_REAR_LEFT = 0x321,  // The left-rear wheel
+        COMMAND_1_REAR_RIGHT = 0x331  // The right-rear wheel
+    } elevator_wheel_tx_command_1_id_t;
+
+    static bool enabled;  // if not enabled, 0 current will be sent in send_elevator_currents
 
     /**
      * @brief contains the target position of both the front wheels and the rear wheels
      * target_position[0] contains the target position of the front wheels
      * target_position[1] contains the target position of the rear wheels
      */
-    int32_t target_position[2];
+    static int32_t target_position[2];
 
     /**
      * @brief set the target position for both the front wheels and the rear wheels
      * @param front_wheel_position      the target position of the front wheels
      * @param rear_wheel_position       the target position of the rear wheels
      */
-    void set_position(int32_t front_wheel_position, int32_t rear_wheel_position);
+    static void set_position(int32_t front_wheel_position, int32_t rear_wheel_position);
 
     /**
      * @brief contains the command CANTxFrames for the four wheels
      * txFrames[0] and txFrames[1] are for the front_left and front_right wheels accordingly
      * txFrames[2] and txFrames[3] are for the left_rear and right_rear wheels accordingly
      */
-    CANTxFrame txFrames[4];
+    static CANTxFrame txFrames[4];
 
     /** Motor Interface **/
     typedef struct {
@@ -54,20 +87,20 @@ public:
 
     } elevator_wheel_t;
 
-    uint16_t PWM;  // the pwm of the current
+    static constexpr uint16_t PWM = 5000;  // the pwm of the current
 
     /**
      * @brief contains the real information of the four wheels
      * elevator_wheels[0] and elevator_wheels[1] are for the front_left and front_right wheels accordingly
      * elevator_wheels[2] and elevator_wheels[3] are for the left_rear and right_rear wheels accordingly
      */
-    elevator_wheel_t elevator_wheels[4];
+    static elevator_wheel_t elevator_wheels[4];
 
     /**
      * @brief Get the feedback (real position, real velocity, real current) of each motor from the driver
      * @return true if success, false otherwise
      */
-    bool get_feedback(CANRxFrame *rxmsg);
+    static bool get_feedback(CANRxFrame *rxmsg);
 
 
     /**
@@ -77,23 +110,10 @@ public:
     bool send_message();
 
     /**
-     * Default constructor
-     */
-    ElevatorInterface() {
-        enabled = false;
-        txFrames[0].SID = FRONT_LEFT;
-        txFrames[1].SID = FRONT_RIGHT;
-        txFrames[2].SID = REAR_LEFT;
-        txFrames[3].SID = REAR_RIGHT;
-    }
-
-    /**
      * @brief set the CAN interface
      * @param can_interface
      */
-    static void set_can_interface (CANInterface* can_interface) {
-        can = can_interface;
-    }
+    static void start(CANInterface* can_interface);
 
 private:
 
