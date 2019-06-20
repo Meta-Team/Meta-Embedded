05c350b0e1d941b2000339b0b361c750ab434605
jasonalson <quokecola@icloud.com>
2019-06-21 02:22:14 +0800

    Merge shoot thread from Hero.

--- dev/vehicle/hero/thread_error_detect.hpp
+++ dev/vehicle/hero/thread_error_detect.hpp
@@ -102,35 +102,56 @@ inline void startupCheckChassisFeedback() {
  */
 
 class ErrorDetectThread : public chibios_rt::BaseStaticThread<1024> {
-
     static constexpr unsigned ERROR_DETECT_THREAD_INTERVAL = 50; // [ms]
 
     void main() final {
 
         setName("detect");
-
+        int bulletStuckCount = 0;
+        int plateStuckCount = 0;
         while (!shouldTerminate()) {
 
-//            if (SYSTIME - MPU6500::last_update_time > 10) {
-//                StateHandler::raiseException(StateHandler::MPU6500_DISCONNECTED);
-//            }
-//
-//            if (SYSTIME - Remote::last_update_time > 30) {
-//                StateHandler::raiseException(StateHandler::REMOTE_DISCONNECTED);
-//            }
-//
-//            for (unsigned i = 0; i < Gimbal::MOTOR_COUNT; i++) {
-//                if (SYSTIME - Gimbal::feedback[i].last_update_time > 5) {
-//                    StateHandler::raiseException(StateHandler::GIMBAL_DISCONNECTED, i);
-//                }
-//            }
-//
-//            for (unsigned i = 0; i < Chassis::MOTOR_COUNT; i++) {
-//                if (SYSTIME - Chassis::feedback[i].last_update_time > 5) {
-//                    StateHandler::raiseException(StateHandler::CHASSIS_DISCONNECTED, i);
-//                }
-//            }
+            if (SYSTIME - MPU6500::last_update_time > 10) {
+                StateHandler::raiseException(StateHandler::MPU6500_DISCONNECTED);
+            }
 
+            if (SYSTIME - Remote::last_update_time > 30) {
+                StateHandler::raiseException(StateHandler::REMOTE_DISCONNECTED);
+            }
+
+            for (unsigned i = 0; i < Gimbal::MOTOR_COUNT; i++) {
+                if (SYSTIME - Gimbal::feedback[i].last_update_time > 5) {
+                    StateHandler::raiseException(StateHandler::GIMBAL_DISCONNECTED, i);
+                }
+            }
+
+            for (unsigned i = 0; i < Chassis::MOTOR_COUNT; i++) {
+                if (SYSTIME - Chassis::feedback[i].last_update_time > 5) {
+                    StateHandler::raiseException(StateHandler::CHASSIS_DISCONNECTED, i);
+                }
+            }
+
+//            if (Shoot::feedback[2].actual_velocity < 1.0 && Shoot::feedback[2].actual_current > ) {
+//                StateHandler::raiseException(StateHandler::BULLET_LOADER_STUCK);
+//            } TODO: Determine if bullet loader is stuck.
+
+// Illustration 1: Use target angle/velocity and the time.
+#if defined(HERO) // Check angle.
+            if (startHandleStuck) bulletStuckCount = 0; // When start handle stuck, count back to zero for the further running.
+            if (bullet_target_angle > Shoot::feedback[2].actual_angle && !startHandleStuck && Shoot::feedback[2].actual_velocity < 4.0f) bulletStuckCount++;
+            if (bulletStuckCount > 10){
+                StateHandler::raiseException(StateHandler::BULLET_LOADER_STUCK); // Time 50ms * 10 times = 0.5s, which should already finished a turn.
+                stuck_angle = Shoot::feedback[2].actual_angle;
+            }
+            // When the velocity is large, the loader is turning happily & smoothly.
+            if (Shoot::feedback[2].actual_velocity > 4.0f) {
+                StateHandler::bulletLoaderSmooth();
+                bulletStuckCount = 0;
+            }
+#elif defined(INFANTRY) || defined(SENTRY) || defined(DRONE) // Though no drones now... Check velocity. which may make the error detect thread could be applied to other robots.
+
+#endif
+// Illustration 2: Use current and velocity. Not start yet
             sleep(TIME_MS2I(ERROR_DETECT_THREAD_INTERVAL));
         }
 
