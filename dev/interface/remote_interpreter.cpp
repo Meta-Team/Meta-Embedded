5d4b442a428f3418512a495fbe48a6ac40d4b0f4
liuzikai <liuzikai@163.com>
2019-07-04 00:56:29 +0800

    Merge Remote bug fix from infantry_v3

--- dev/interface/remote_interpreter.cpp
+++ dev/interface/remote_interpreter.cpp
@@ -3,7 +3,16 @@
 // Inspired by ChibiStudio template code.
 //
 
+/**
+ * @file    remote_interpreter.cpp
+ * @brief   Module to receive date from DR16 receiver and interpret data to specific formats
+ *
+ * @addtogroup remote
+ * @{
+ */
+
 #include "remote_interpreter.h"
+#include "led.h"
 
 /**
  * Hardware configurations.
@@ -12,102 +21,156 @@
  *  Speed: 100000
  **/
 
-
 Remote::rc_t Remote::rc;
 Remote::mouse_t Remote::mouse;
 Remote::keyboard_t Remote::key;
 time_msecs_t Remote::last_update_time;
+
+#if REMOTE_USE_EVENTS
+// See macro EVENTSOURCE_DECL() for initialization style
+event_source_t Remote::s_change_event = {(event_listener_t *)(&Remote::s_change_event)};
+event_source_t Remote::mouse_press_event = {(event_listener_t *)(&Remote::mouse_press_event)};
+event_source_t Remote::mouse_release_event = {(event_listener_t *)(&Remote::mouse_release_event)};
+event_source_t Remote::key_press_event = {(event_listener_t *)(&Remote::key_press_event)};
+event_source_t Remote::key_release_event = {(event_listener_t *)(&Remote::key_release_event)};
+#endif
+
 char Remote::rx_buf_[Remote::RX_FRAME_SIZE];
-constexpr UARTConfig Remote::REMOTE_UART_CONFIG;
+UARTConfig Remote::REMOTE_UART_CONFIG = {
+        nullptr,
+        nullptr,
+        uart_received_callback_, // callback function when the buffer is filled
+        nullptr,
+        nullptr,
+        100000, // speed
+        USART_CR1_PCE,
+        0,
+        0,
+};
 
 /**
- * Helper macro to log user behavior
- */
-#define LOG_PRESS_AND_RELEASE(old_val, new_val, name) { \
-    if (old_val != new_val) { \
-        if (new_val) LOG_USER("press %s", name); \
-        else LOG_USER("release %s", name); \
-    } \
-}
-
-/**
- * @brief callback function when the buffer is filled
- * @param uartp
+ * @note DO NOT use printf, LOG, etc. in this function since it's an ISR callback.
  */
 void Remote::uart_received_callback_(UARTDriver *uartp) {
 
-//    chSysLockFromISR(); // --- Enter Critical Zone ---
+    chSysLockFromISR();  /// ---------------------------------- Enter Critical Zone ----------------------------------
+
+    /// RC
 
     rc.ch0 = (((rx_buf_[0] | rx_buf_[1] << 8) & 0x07FF) - 1024.0f) / 660.0f;
     rc.ch1 = (((rx_buf_[1] >> 3 | rx_buf_[2] << 5) & 0x07FF) - 1024.0f) / 660.0f;
     rc.ch2 = (((rx_buf_[2] >> 6 | rx_buf_[3] << 2 | rx_buf_[4] << 10) & 0x07FF) - 1024.0f) / 660.0f;
     rc.ch3 = (((rx_buf_[4] >> 1 | rx_buf_[5] << 7) & 0x07FF) - 1024.0f) / 660.0f;
 
-#if REMOTE_ENABLE_USER_LOG
+    auto s1 = (rc_status_t) ((rx_buf_[5] >> 6) & 0x0003);
+    auto s2 = (rc_status_t) ((rx_buf_[5] >> 4) & 0x0003);
 
-    rc_t rc_;
-    rc_.s1 = (rc_status_t) ((rx_buf_[5] >> 6) & 0x0003);
-    rc_.s2 = (rc_status_t) ((rx_buf_[5] >> 4) & 0x0003);
-    if (rc_.s1 != rc.s1) LOG_USER("switch s1 from %d to %d", rc.s1, rc_.s1);
-    if (rc_.s2 != rc.s2) LOG_USER("switch s2 from %d to %d", rc.s2, rc_.s2);
-    rc = rc_;
+#if REMOTE_USE_EVENTS
 
-    mouse_t mouse_;
-    mouse_.press_left = (bool) rx_buf_[12];
-    mouse_.press_right = (bool) rx_buf_[13];
-    LOG_PRESS_AND_RELEASE(mouse.press_left, mouse_.press_left, "left");
-    LOG_PRESS_AND_RELEASE(mouse.press_right, mouse_.press_right, "right");
-    mouse = mouse_;
+    if (s1 != rc.s1 || s2 != rc.s2) {
+        chEvtBroadcastI(&s_change_event);
+    }
 
-    keyboard_t key_;
-    key_._key_code = static_cast<uint16_t>(rx_buf_[14] | rx_buf_[15] << 8);
-    LOG_PRESS_AND_RELEASE(key.w, key_.w, "W");
-    LOG_PRESS_AND_RELEASE(key.s, key_.s, "S");
-    LOG_PRESS_AND_RELEASE(key.a, key_.a, "A");
-    LOG_PRESS_AND_RELEASE(key.d, key_.d, "D");
-    LOG_PRESS_AND_RELEASE(key.shift, key_.shift, "Shift");
-    LOG_PRESS_AND_RELEASE(key.ctrl, key_.ctrl, "Ctrl");
-    LOG_PRESS_AND_RELEASE(key.q, key_.q, "Q");
-    LOG_PRESS_AND_RELEASE(key.e, key_.e, "E");
-    LOG_PRESS_AND_RELEASE(key.r, key_.r, "R");
-    LOG_PRESS_AND_RELEASE(key.f, key_.f, "f");
-    LOG_PRESS_AND_RELEASE(key.g, key_.g, "G");
-    LOG_PRESS_AND_RELEASE(key.z, key_.z, "Z");
-    LOG_PRESS_AND_RELEASE(key.x, key_.x, "X");
-    LOG_PRESS_AND_RELEASE(key.c, key_.c, "C");
-    LOG_PRESS_AND_RELEASE(key.v, key_.v, "V");
-    LOG_PRESS_AND_RELEASE(key.b, key_.b, "B");
-    key = key_;
+#endif
 
-#else
+    rc.s1 = s1;
+    rc.s2 = s2;
 
-    rc.s1 = (rc_status_t) ((rx_buf_[5] >> 6) & 0x0003);
-    rc.s2 = (rc_status_t) ((rx_buf_[5] >> 4) & 0x0003);
+    rc.wheel = ((((int16_t) rx_buf_[16] | ((int16_t) rx_buf_[17] << 8)) & 0x07FF) - 1024.0f) / 660.0f;
+
+
+    /// Mouse
 
     mouse.x = ((int16_t) (rx_buf_[6] | rx_buf_[7] << 8)) / 32767.0f;
     mouse.y = ((int16_t) (rx_buf_[8] | rx_buf_[9] << 8)) / 32767.0f;
     mouse.z = ((int16_t) (rx_buf_[10] | rx_buf_[11] << 8)) / 32767.0f;
 
-    mouse.press_left = (bool) rx_buf_[12];
-    mouse.press_right = (bool) rx_buf_[13];
+    auto press_left = (bool) rx_buf_[12];
+    auto press_right = (bool) rx_buf_[13];
 
-    key._key_code = static_cast<uint16_t>(rx_buf_[14] | rx_buf_[15] << 8);
+#if REMOTE_USE_EVENTS
+
+    /**
+     * Similar to key_code below.
+     */
+
+    uint32_t new_mouse_code =       press_left << MOUSE_LEFT |       press_right << MOUSE_RIGHT;
+    uint32_t old_mouse_code = mouse.press_left << MOUSE_LEFT | mouse.press_right << MOUSE_RIGHT;
+    uint32_t diff_mouse_code = new_mouse_code ^ old_mouse_code;
+    if (diff_mouse_code) {
+        chEvtBroadcastFlagsI(&mouse_press_event, new_mouse_code & diff_mouse_code);
+        chEvtBroadcastFlagsI(&mouse_release_event, old_mouse_code & diff_mouse_code);
+    }
 
 #endif
+
+    mouse.press_left = press_left;
+    mouse.press_right = press_right;
+
+
+    // Key
+
+    uint32_t key_code_ = static_cast<uint16_t>(rx_buf_[14] | rx_buf_[15] << 8);
+
+#if REMOTE_USE_EVENTS
+
+    /**
+     * Explanation of using bitwise operators to generate event flags:
+     *
+     * 1. diff_key_code                 - XOR marks the different bits as 1 into diff_key_code
+     * 2. key_code_ & diff_key_code     - Unchanged bits are masked to 0. Released keys are also 0. 1 bits are those
+     *                                    newly pressed.
+     * 3. key.key_code_ & diff_key_code - Unchanged bits are masked to 0. Originally non-pressed keys (now changed to 1)
+     *                                    are also 0. Originally pressed keys (now change to 0) are 1. So 1 bits are
+     *                                    those newly released.
+     * Notice that 2 and 3 are inverse of each other.
+     *
+     *
+     * For example, W releases and S presses
+     *
+     * |               | W | S | A | D |
+     * | key.key_code_ | 1 | 0 | 1 | 0 |
+     * | key_code_     | 0 | 1 | 1 | 0 |
+     * | diff_key_code | 1 | 1 | 0 | 0 |
+     * | pressed flag  | 0 | 1 | 0 | 0 | (S key has pressed event)
+     * | released flag | 1 | 0 | 0 | 0 | (W key has released event)
+     */
+
+    uint32_t diff_key_code = key_code_ ^ key.key_code_;
+    if (diff_key_code) {
+        chEvtBroadcastFlagsI(&key_press_event, key_code_ & diff_key_code);
+        chEvtBroadcastFlagsI(&key_release_event, key.key_code_ & diff_key_code);
+    }
+
+#endif
+
+    key.key_code_ = key_code_;
+
+
+    // Final
 
     last_update_time = SYSTIME;
 
     // Restart the receive
-    uartStartReceive(uartp, RX_FRAME_SIZE, rx_buf_);
+    uartStartReceiveI(uartp, RX_FRAME_SIZE, rx_buf_);
 
-//    chSysUnlockFromISR(); // --- Exit Critical Zone ---
+    chSysUnlockFromISR();  /// ---------------------------------- Exit Critical Zone ----------------------------------
 }
 
-/**
- * @brief Initialize a remote interpreter instance and return its pointer.
- * @return the pointer of remote interpreter.
- */
-void Remote::start_receive() {
-    uartStart(&REMOTE_UART_DRIVER, &REMOTE_UART_CONFIG);
+void Remote::uart_synchronize() {
+    // Wait for no input in 5 ms, to avoid one receive starting from the middle of a frame
+    while (true) {
+        // For unknown reason, uartReceiveTimeout() seems not to wait for additional bytes if byte_received > 1
+        size_t byte_received = 1;
+        msg_t ret = uartReceiveTimeout(&REMOTE_UART_DRIVER, &byte_received, rx_buf_, TIME_MS2I(5));
+        if (ret == MSG_TIMEOUT) break;
+    }
     uartStartReceive(&REMOTE_UART_DRIVER, RX_FRAME_SIZE, rx_buf_);
 }
+
+void Remote::start() {
+    uartStart(&REMOTE_UART_DRIVER, &REMOTE_UART_CONFIG);
+    uart_synchronize();
+}
+
+/** @} */
