1550f3649c342102936c3c7d2198ca262c5a6a7f
jasonalson <QuokeCola@iCloud.com>
2019-07-07 01:08:32 +0800

    clean up the unit_test folder... looks tider...

---
+++ dev/debug/unit_tests/pa_chassis/pa_chassis.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 钱晨 on 2019-07-07.
+//
+
+#include "pa_chassis.h"
---
+++ dev/debug/unit_tests/pa_chassis/pa_chassis.h
@@ -0,0 +1,14 @@
+//
+// Created by 钱晨 on 2019-07-07.
+//
+
+#ifndef META_INFANTRY_PA_CHASSIS_H
+#define META_INFANTRY_PA_CHASSIS_H
+
+
+class pa_chassis {
+
+};
+
+
+#endif //META_INFANTRY_PA_CHASSIS_H
