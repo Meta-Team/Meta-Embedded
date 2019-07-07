eccb01b52d4dca4ba24f0846a1571bb4c61747c9
liuzikai <liuzikai@163.com>
2019-07-07 19:21:43 +0800

    Halfdone small revisions.

--- dev/vehicle/infantry/user.h
+++ dev/vehicle/infantry/user.h
@@ -25,7 +25,7 @@ private:
 
     /// Gimbal Parameters
     static constexpr float GIMBAL_RC_YAW_MAX_SPEED = 90;  // [degree/s]
-    static constexpr float GIMBAL_PC_YAW_SENSITIVITY[2] = {20000, 54000};     // rotation speed when mouse moves fastest [degree/s]
+    static constexpr float GIMBAL_PC_YAW_SENSITIVITY[3] = {20000, 54000, 100000};  // [Ctrl, Normal, Shift] [degree/s]
 
     static constexpr float GIMBAL_PC_PITCH_SENSITIVITY = 12000;   // rotation speed when mouse moves fastest [degree/s]
     static constexpr float GIMBAL_PITCH_MIN_ANGLE = -10; // down range for pitch [degree]
@@ -38,20 +38,20 @@ private:
     static constexpr float SHOOT_COMMON_DUTY_CYCLE = 0.8;
 
     /// Chassis Parameters
-    static constexpr float CHASSIS_COMMON_VX = 1200.0f;  // [mm/s]
-    static constexpr float CHASSIS_COMMON_VY = 1200.0f;  // [mm/s]
+    static constexpr float CHASSIS_COMMON_VX = 1000.0f;  // [mm/s]
+    static constexpr float CHASSIS_COMMON_VY = 1000.0f;  // [mm/s]
 
-    static constexpr float CHASSIS_PC_CTRL_RATIO = 0.5f;  // 50% when Ctrl is pressed
+    static constexpr float CHASSIS_PC_SHIFT_RATIO = 1.5f;  // 150% when Shift is pressed
+    static constexpr float CHASSIS_PC_CTRL_RATIO = 0.5f;   // 50% when Ctrl is pressed
 
 
     /// Thread
     static constexpr unsigned USER_THREAD_INTERVAL = 7;  // [ms]
 
     class UserThread : public chibios_rt::BaseStaticThread<512> {
-        float gimbal_rc_yaw_target_angle = 0;
-        float gimbal_pc_yaw_target_angle = 0;
+
+        float gimbal_yaw_target_angle = 0;
         float gimbal_pc_pitch_target_angle = 0;
-        unsigned gimbal_pc_yaw_sensitivity_level = 1;
 
         bool pc_z_pressed = false;
         bool pc_x_pressed = false;
@@ -62,6 +62,7 @@ private:
 
         bool pc_mouse_left_pressed = false;
         bool pc_mouse_right_pressed = false;
+
         void main() final;
     };
 
