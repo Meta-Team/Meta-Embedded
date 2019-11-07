25a3d51ad1440e83f85ba5a9507657c31e92838e
QuokeCola <QuokeCola@iCloud.com>
2019-11-08 01:14:05 +0800

    Add light flash.

--- dev/debug/Unit_Tests/ut_sliprin.cpp
+++ dev/debug/Unit_Tests/ut_sliprin.cpp
@@ -6,15 +6,13 @@
 
 #include "buzzer.h"
 #include "led.h"
-#include "common_macro.h"
-#include "debug/shell/shell.h"
+#include "shell.h"
 
 #include "pid_controller.hpp"
 #include "can_interface.h"
 #include "interface/chassis_interface.h"
 #include "interface/gimbal_interface.h"
 
-unsigned const INTERVAL = 1;
 using namespace chibios_rt;
 
 #if defined(BOARD_RM_2018_A)
@@ -33,12 +31,12 @@ CANInterface can1(&CAND1);
     {CHASSIS_PID_V2I_KP, CHASSIS_PID_V2I_KI, CHASSIS_PID_V2I_KD, \
     CHASSIS_PID_V2I_I_LIMIT, CHASSIS_PID_V2I_OUT_LIMIT}
 
-float target_velocity = 1.0f;
+float target_velocity = 0.0f;
 
 class CurrentSendThread : public chibios_rt::BaseStaticThread<512> {
     void main() final {
-
-        bool a = false;
+        int time = SYSTIME;
+        bool status = false;
 
         PIDController lv2i;
         PIDController rv2i;
@@ -46,20 +44,49 @@ class CurrentSendThread : public chibios_rt::BaseStaticThread<512> {
         lv2i.change_parameters(CHASSIS_PID_V2I_PARAMS);
         rv2i.change_parameters(CHASSIS_PID_V2I_PARAMS);
 
-        float last_log_angle;
-        LED::all_off();
         while (!shouldTerminate()) {
-            LED::led_on(3);
             ChassisIF::target_current[0] = (int) lv2i.calc(ChassisIF::feedback[0].actual_velocity,target_velocity);
             ChassisIF::target_current[1] = (int) rv2i.calc(ChassisIF::feedback[1].actual_velocity,-target_velocity);
             ChassisIF::send_chassis_currents();
-            Shell::printf("!cv,%u,%.2f" SHELL_NEWLINE_STR,
-                          TIME_I2MS(chibios_rt::System::getTime()),
-                          ChassisIF::feedback[0].actual_velocity);
+            if (SYSTIME-time>100) {
+                if(!status) {
+                    LED::led_on(5);
+                    time = SYSTIME;
+                    status = !status;
+                } else {
+                    LED::led_off(5);
+                    time = SYSTIME;
+                    status = !status;
+                }
+            }
         }
         sleep(TIME_MS2I(1));
     }
 }currentSendThread;
+
+class FeedbackThread : public chibios_rt::BaseStaticThread<512> {
+    int time = SYSTIME;
+    bool status = false;
+    void main() final {
+        while (!shouldTerminate()){
+            Shell::printf("!cv,%u,%.2f" SHELL_NEWLINE_STR,
+                          TIME_I2MS(chibios_rt::System::getTime()),
+                          ChassisIF::feedback[0].actual_velocity);
+            if (SYSTIME-time>100) {
+                if(!status) {
+                    LED::led_on(6);
+                    time = SYSTIME;
+                    status = !status;
+                } else {
+                    LED::led_off(6);
+                    time = SYSTIME;
+                    status = !status;
+                }
+            }
+            sleep(TIME_MS2I(100));
+        }
+    }
+}feedbackThread;
 
 static void cmd_set_velocity (BaseSequentialStream *chp, int argc, char *argv[]) {
     (void) argv;
@@ -77,33 +104,34 @@ ShellCommand ControllerCommands[] = {
 int main() {
 
     halInit();
-    chibios_rt::System::init();
-    Shell::start(LOWPRIO);
+    System::init();
+    Shell::start(LOWPRIO +5);
     Shell::addCommands(ControllerCommands);
-    palSetPadMode(GPIOH, GPIOH_POWER1_CTRL, PAL_MODE_OUTPUT_PUSHPULL);
-    palSetPad(GPIOH, GPIOH_POWER1_CTRL);
 
     LED::all_off();
     can1.start(HIGHPRIO - 1);
     ChassisIF::init(&can1);
     time_msecs_t t1 = SYSTIME;
+
+    /**-----check can error------*/
     while (WITHIN_RECENT_TIME(t1, 100)) {
         if (WITHIN_RECENT_TIME(can1.last_error_time, 5)) {  // can error occurs
             t1 = SYSTIME;  // reset the counter
         }
     }
     LED::led_on(1);
-//    time_msecs_t t = SYSTIME;
-//    while (WITHIN_RECENT_TIME(t, 20)) {
-//        if (not WITHIN_RECENT_TIME(ChassisIF::feedback[ChassisIF::FR].last_update_time, 5)) {
-//            // No feedback in last 5 ms (normal 1 ms)
-//            LOG_ERR("Startup - Chassis FR offline.");
-//            t = SYSTIME;  // reset the counter
-//        }
-//    }
-    LED::led_on(2);
 
-    currentSendThread.start(HIGHPRIO - 2);
-    Buzzer::play_sound(Buzzer::sound_startup_intel, HIGHPRIO - 3);  // Now play the startup sound
+    /**---check feedback error---*/
+    time_msecs_t t = SYSTIME;
+    while (WITHIN_RECENT_TIME(t, 20)) {
+        if (not WITHIN_RECENT_TIME(ChassisIF::feedback[ChassisIF::FR].last_update_time, 5)) {
+            // No feedback in last 5 ms (normal 1 ms)
+            LOG_ERR("Startup - Chassis FR offline.");
+            t = SYSTIME;  // reset the counter
+        }
+    }
+    LED::led_on(2);
+    currentSendThread.start(NORMALPRIO + 2);
+    feedbackThread.start(NORMALPRIO+1);
     return 0;
 }
