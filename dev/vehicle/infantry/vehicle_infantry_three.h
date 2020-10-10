25b2540e83da4b1068650142e3d846ce7788e8e4
Qian Chen <QuokeCola@iCloud.com>
2020-10-11 02:47:15 +0800

    revision

--- dev/vehicle/infantry/vehicle_infantry_three.h
+++ dev/vehicle/infantry/vehicle_infantry_three.h
@@ -11,7 +11,7 @@
 
 // Raw angle of yaw and pitch when gimbal points straight forward.
 //   Note: the program will echo the raw angles of yaw and pitch as the program starts
-#define GIMBAL_YAW_FRONT_ANGLE_RAW 4456
+#define GIMBAL_YAW_FRONT_ANGLE_RAW 4457
 #define GIMBAL_PITCH_FRONT_ANGLE_RAW 2866  // of no use now
 
 #define MPU6500_STORED_GYRO_BIAS {0.491007685, -1.024637818, -0.371067702}
