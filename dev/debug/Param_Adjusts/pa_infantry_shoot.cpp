366e590fd141d257f30ccaa9da24fe3ede0f0d34
Qian Chen <QuokeCola@iCloud.com>
2021-01-11 22:02:06 +0800

    Add pa_infantry_shoot for shooting tests.

--- dev/debug/Param_Adjusts/pa_infantry_shoot.cpp
+++ dev/debug/Param_Adjusts/pa_infantry_shoot.cpp
@@ -9,6 +9,11 @@
 #include "ch.hpp"
 #include "hal.h"
 
+#include "ahrs.h"
+#include "sd_card_interface.h"
+
+#include "remote_interpreter.h"
+
 #include "led.h"
 #include "debug/shell/shell.h"
 #include "interface/gimbal_interface.h"
@@ -16,18 +21,22 @@
 #include "vehicle/infantry/vehicle_infantry.h"
 #include "scheduler/gimbal_scheduler.h"
 
-#include "ahrs.h"
+#include "scheduler/buzzer_scheduler.h"
+
+#define MPU6500_STORED_GYRO_BIAS {1.142140388f, -1.022603631f, 0.125763729f}
 // Other headers here
 
 using namespace chibios_rt;
 
 CANInterface::motor_feedback_t *feedback[4];
 int *targetCurrent[2];
+float loaderV;
 
 PIDController::pid_params_t upper_pid;
 PIDController::pid_params_t lower_pid;
 
 PIDController v2iController[2];
+PIDController loaderPID;
 
 CANInterface can1(&CAND1);
 CANInterface can2(&CAND2);
@@ -56,13 +65,24 @@ static GimbalIF::motor_can_config_t GIMBAL_MOTOR_CONFIG_[GimbalIF::MOTOR_COUNT] 
  * @param argc
  * @param argv
  */
+static void cmd_gimbal_enable_feedback(BaseSequentialStream *chp, int argc, char *argv[]) {
+    (void) argv;
+    if (argc != 2 || (*argv[0] != '0' && *argv[0] != '1') || (*argv[1] != '0' && *argv[1] != '1')) {
+        shellUsage(chp, "g_enable_fb yaw(0/1) pitch(0/1)");
+        return;
+    }
+    enable_l_feedback = *argv[0] - '0';
+    enable_u_feedback = *argv[1] - '0';
+}
+
 static void cmd_set_target(BaseSequentialStream *chp, int argc, char *argv[]) {
     (void) argv;
     if (argc != 2) {
         shellUsage(chp, "g_set_v yaw_velocity pitch_velocity");
         return;
     }
-
+    targetVelocity[0] = Shell::atof(argv[0]);
+    targetVelocity[1] = Shell::atof(argv[1]);
 }
 
 static void cmd_set_param(BaseSequentialStream *chp, int argc, char *argv[]) {
@@ -93,6 +113,7 @@ static void cmd_set_param(BaseSequentialStream *chp, int argc, char *argv[]) {
 ShellCommand ShellCommands[] = {
         {"g_set_v", cmd_set_target},
         {"g_set_params", cmd_set_param},
+        {"g_enable_fb", cmd_gimbal_enable_feedback},
         {nullptr,    nullptr}
 };
 
@@ -101,10 +122,20 @@ ShellCommand ShellCommands[] = {
 class SKDThread : public BaseStaticThread <512> {
 private:
     void main() final {
+
         setName("SKDThread");
         while (!shouldTerminate()) {
+            if (!ABS_IN_RANGE(Remote::rc.wheel,0.2)) {
+                loaderV = 100.0f;
+                targetVelocity[0] = targetVelocity[1] = Remote::rc.wheel * 2800.0f;
+            } else {
+                loaderV = 0.0f;
+                targetVelocity[0] = targetVelocity[1] = 0.0f;
+            }
+            GimbalSKD::set_target_angle(0.0f, 0.0f);
             *GimbalIF::target_current[GimbalIF::FW_LEFT]= (int)v2iController[0].calc(GimbalIF::feedback[GimbalIF::FW_LEFT]->actual_velocity, targetVelocity[0]);
-            *GimbalIF::target_current[GimbalIF::FW_RIGHT]= (int)v2iController[0].calc(GimbalIF::feedback[GimbalIF::FW_RIGHT]->actual_velocity, targetVelocity[0]);
+            *GimbalIF::target_current[GimbalIF::FW_RIGHT]= (int)v2iController[1].calc(GimbalIF::feedback[GimbalIF::FW_RIGHT]->actual_velocity, -targetVelocity[0]);
+            *GimbalIF::target_current[GimbalIF::BULLET] = (int)loaderPID.calc(GimbalIF::feedback[GimbalIF::BULLET]->actual_velocity, loaderV);
             sleep(TIME_MS2I(5));
         }
     }
@@ -142,6 +173,23 @@ int main(void) {
     Shell::start(HIGHPRIO);
     Shell::addCommands(ShellCommands);
 
+    can1.start(HIGHPRIO - 6, HIGHPRIO - 7);
+    can2.start(HIGHPRIO - 8, HIGHPRIO - 9);
+
+    Remote::start();
+
+    /*** Set on board ahrs ***/
+    Vector3D ahrs_bias;
+    if (SDCard::get_data(MPU6500_BIAS_DATA_ID, &ahrs_bias, sizeof(ahrs_bias)) == SDCard::OK) {
+        ahrs.load_calibration_data(ahrs_bias);
+        LOG("Use AHRS bias in SD Card");
+    } else {
+        ahrs.load_calibration_data(MPU6500_STORED_GYRO_BIAS);
+        LOG_WARN("Use default AHRS bias");
+    }
+    ahrs.load_calibration_data({-0.644649505f, -0.619945943f, 0.173617705f});
+    ahrs.start(ON_BOARD_AHRS_MATRIX_, THREAD_MPU_PRIO, THREAD_IST_PRIO, THREAD_AHRS_PRIO);
+
     GimbalIF::init(&can1, &can2, GIMBAL_MOTOR_CONFIG_, GIMBAL_YAW_FRONT_ANGLE_RAW, GIMBAL_PITCH_FRONT_ANGLE_RAW);
     chThdSleepMilliseconds(2000);  // wait for C610 to be online and friction wheel to reset
 
@@ -152,10 +200,16 @@ int main(void) {
     GimbalSKD::set_yaw_restriction(GIMBAL_RESTRICT_YAW_MIN_ANGLE, GIMBAL_RESTRICT_YAW_MAX_ANGLE,
                                    GIMBAL_RESTRICT_YAW_VELOCITY);
 
-    GimbalSKD::set_target_angle(0.0f, 0.0f);
-
+    GimbalSKD::set_mode(GimbalSKD::ABS_ANGLE_MODE);
     skdThread.start(NORMALPRIO + 1);
     feedbackThread.start(NORMALPRIO + 2);
+
+    v2iController[0].change_parameters(SHOOT_PID_FW_LEFT_V2I_PARAMS);
+    v2iController[1].change_parameters(SHOOT_PID_FW_RIGHT_V2I_PARAMS);
+    loaderPID.change_parameters(SHOOT_PID_BULLET_LOADER_V2I_PARAMS);
+
+    BuzzerSKD::init(NORMALPRIO + 4);
+    BuzzerSKD::play_sound(BuzzerSKD::sound_nyan_cat);
 
 #if CH_CFG_NO_IDLE_THREAD // see chconf.h for what this #define means
     // ChibiOS idle thread has been disabled, main() should implement infinite loop
