eed80d254a349db6b97423babc3acfee3e757b27
quokecola <quokecola@icloud.com>
2021-10-29 18:56:33 -0500

    Add haptic device related code.

---
+++ dev/scheduler/haptic_scheduler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 钱晨 on 10/29/21.
+//
+
+#include "haptic_scheduler.h"
---
+++ dev/scheduler/haptic_scheduler.h
@@ -0,0 +1,14 @@
+//
+// Created by 钱晨 on 10/29/21.
+//
+
+#ifndef META_INFANTRY_HAPTIC_SCHEDULER_H
+#define META_INFANTRY_HAPTIC_SCHEDULER_H
+
+
+class haptic_scheduler {
+
+};
+
+
+#endif //META_INFANTRY_HAPTIC_SCHEDULER_H
