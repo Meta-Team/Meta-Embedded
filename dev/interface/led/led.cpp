0812d8f51a47496ecc401e6074b42da03d637f91
QuokeCola <quokecola@icloud.com>
2023-01-26 00:02:34 -0500

    Temporary disable buzzer when system halt.

--- dev/interface/led/led.cpp
+++ dev/interface/led/led.cpp
@@ -12,7 +12,7 @@
  */
 
 #include "led.h"
-#include "buzzer_scheduler.h"
+//#include "buzzer_scheduler.h"
 
 extern "C" {
 /**
@@ -22,45 +22,50 @@ extern "C" {
 void set_led_when_halt(void) {
 
     /// Turn on red LED when halt
+#if defined(BOARD_RM_C)
+    LED_PAD_ON(GPIOH, GPIOH_LED_R);    // LED_RED_ON
+    LED_PAD_OFF(GPIOH, GPIOH_LED_G); // LED_GREEN_OFF
+#else
     LED_PAD_ON(GPIOF, GPIOE_LED_RED);    // LED_RED_ON
     LED_PAD_OFF(GPIOF, GPIOF_LED_GREEN); // LED_GREEN_OFF
+#endif
 
 
 
-    // Now system is unlocked
-
-    PWMConfig pwm_config = {
-            1000000,
-            1000000, // Default playing_note: 1Hz
-            nullptr,
-            {
-                    {PWM_OUTPUT_ACTIVE_HIGH, nullptr},  // it's all CH1 for current support boards
-                    {PWM_OUTPUT_DISABLED, nullptr},
-                    {PWM_OUTPUT_DISABLED, nullptr},
-                    {PWM_OUTPUT_DISABLED, nullptr}
-            },
-            0,
-            0
-    };
-
-    if ((BUZZER_PWM_DRIVER.state != PWM_STOP) && (BUZZER_PWM_DRIVER.state != PWM_READY)) {
-
-        // Unlock system if in system locked state
-        // The following code is extract from chSysUnconditionalUnlock() with debug check eliminated
-        if (!port_irq_enabled(port_get_irq_status())) {  // is in locked state
-            _stats_stop_measure_crit_thd();
-            port_unlock();
-        }
-        // Hopefully no content switch will happen here...
-
-        pwmStart(&BUZZER_PWM_DRIVER, &pwm_config);  // require unlock state
-
-        chSysUnconditionalLock();  // recover
-
-    }  // if PWM has already started, do nothing
-
-    pwmChangePeriod(&BUZZER_PWM_DRIVER, 1000000 / 349);
-    pwmEnableChannel(&BUZZER_PWM_DRIVER, 0, PWM_PERCENTAGE_TO_WIDTH(&BUZZER_PWM_DRIVER, 5000)); // 50%
+//    // Now system is unlocked
+//
+//    PWMConfig pwm_config = {
+//            1000000,
+//            1000000, // Default playing_note: 1Hz
+//            nullptr,
+//            {
+//                    {PWM_OUTPUT_ACTIVE_HIGH, nullptr},  // it's all CH1 for current support boards
+//                    {PWM_OUTPUT_DISABLED, nullptr},
+//                    {PWM_OUTPUT_DISABLED, nullptr},
+//                    {PWM_OUTPUT_DISABLED, nullptr}
+//            },
+//            0,
+//            0
+//    };
+//
+//    if ((BUZZER_PWM_DRIVER.state != PWM_STOP) && (BUZZER_PWM_DRIVER.state != PWM_READY)) {
+//
+//        // Unlock system if in system locked state
+//        // The following code is extract from chSysUnconditionalUnlock() with debug check eliminated
+//        if (!port_irq_enabled(port_get_irq_status())) {  // is in locked state
+//            _stats_stop_measure_crit_thd();
+//            port_unlock();
+//        }
+//        // Hopefully no content switch will happen here...
+//
+//        pwmStart(&BUZZER_PWM_DRIVER, &pwm_config);  // require unlock state
+//
+//        chSysUnconditionalLock();  // recover
+//
+//    }  // if PWM has already started, do nothing
+//
+//    pwmChangePeriod(&BUZZER_PWM_DRIVER, 1000000 / 349);
+//    pwmEnableChannel(&BUZZER_PWM_DRIVER, 0, PWM_PERCENTAGE_TO_WIDTH(&BUZZER_PWM_DRIVER, 5000)); // 50%
 
     chSysUnconditionalLock();  // lock in following infinity loop in chSysHalt()
 }
